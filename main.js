a1b371c4199325fde27a65846e7ae935a7e0f0df
kendallli587 <kendallli587@gmail.com>
2022-07-05 10:34:54 -0600

    html

---
+++ main.js
@@ -0,0 +1,14 @@
+// Triangle Perimeter Calculator
+
+// Button Event Listener
+document.getElementById("btn").addEventListener("click", getUserInput);
+
+// Main Event Function on Button Press
+function getUserInput() {
+    let x1 = document.getElementById("x1");
+    let x2 = document.getElementById("x2");
+    let x3 = document.getElementById("x3");
+    let y1 = document.getElementById("y1");
+    let y2 = document.getElementById("y2");
+    let y3 = document.getElementById("y3");
+}
